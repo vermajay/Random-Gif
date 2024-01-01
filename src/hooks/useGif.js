146c3bd19f2242ee2416254419896799942474ef
vermajay <vermajay6604@gmail.com>
2024-01-01 13:16:17 +0530

    solve callback issues

--- src/hooks/useGif.js
+++ src/hooks/useGif.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';    //axios is another way to make api calls - more secure than fetch api call
 
 const API_KEY = process.env.REACT_APP_GIPHY_API_KEY;   //process to get value from environment variables
@@ -9,13 +9,13 @@ const useGif = (tag) => {
     const [gif, setGif] = useState('');
     const [loading, setLoading] = useState(false);
 
-    async function fetchData(){
+    const fetchData = useCallback(async()=>{
         setLoading(true);
         const {data} = await axios.get(tag ? `${url}&tag=${tag}` : url);
         const imageSource = data.data.images.downsized_large.url;
         setGif(imageSource);
         setLoading(false);
-    }
+    }, [tag])
 
     useEffect(()=>{
         fetchData();   //call the api automatically when the app first renders
